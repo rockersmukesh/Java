381cbf2a064d1d73060f23d26ce031752a03aaa9
Rockers Mukesh <110319782+rockersmukesh@users.noreply.github.com>
2023-11-12 17:19:53 +0000

    git add methodoverloading.java

---
+++ methodoverloading.java
@@ -0,0 +1,24 @@
+class calc
+{
+    public void add(int a,int b)
+    {
+        int c;
+        c=a+b;
+        System.out.println("Addition="+c);
+    }
+    public void mul(double a,double b)
+    {
+        double c;
+        c=a*b;
+        System.out.println("Multiplication="+c);
+  }
+}
+class methodoverloading
+{
+    public static void main(String[] args)
+    {
+        calc obj=new calc();
+        obj.add(10,20);
+        obj.mul(10.5,20.5);
+    }
+}
