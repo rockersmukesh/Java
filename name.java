76b8e93517c37db8916e3a9d3a9980096f42cca8
Rockers Mukesh <110319782+rockersmukesh@users.noreply.github.com>
2023-11-22 09:57:00 +0530

    Create name.java

---
+++ name.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class name
+{
+	public static void main(String[] args)
+	{
+		String a;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter Your Name:");
+		a = sc.nextLine();
+		System.out.println("Your Name is "+a);
+	}
+}
